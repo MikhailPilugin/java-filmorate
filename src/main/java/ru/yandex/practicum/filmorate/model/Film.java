17cfb51e4482067d810ac01d625a80055c209ccd
Mikhail_Mac <mikhail-dssv@rambler.ru>
2023-03-27 10:11:44 +0300

    Добавил классы User и Film

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+    protected int id;
+    protected String name;
+    protected String description;
+    protected LocalDate releaseDate;
+    protected Duration duration;
+}
