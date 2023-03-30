a1fd6b76637332cac04c18d5beed8e013bb5aa0b
Mikhail_Mac <mikhail-dssv@rambler.ru>
2023-03-30 17:52:12 +0300

    Рефакторинг UserController для тестов в GitHub

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -50,7 +50,7 @@ public class UserController {
             log.info("Добавлен новый пользователь: " + user.getLogin());
 
         // пользователи уже есть
-        } else if ( !isLoginContainsSpace && isLoginNotEmpty && isBirthDayBeforeToday) {
+        } else if (!isLoginContainsSpace && isLoginNotEmpty && isBirthDayBeforeToday) {
 
             for (Map.Entry<Integer, User> integerUserEntry : userMap.entrySet()) {
                 String userLogin = integerUserEntry.getValue().getLogin();
