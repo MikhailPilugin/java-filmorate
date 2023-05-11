7d47409d72739d66551db18ba0c22ce4ef473d16
Mikhail Pilugin <tonopod8@yandex.ru>
2023-05-11 09:35:06 +0300

    Chtckstyle на github

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
-class FilmoRateApplicationTests {
+class FilmorateApplicationTests {
 	private final UserDbStorage userStorage;
 
 	@Test
