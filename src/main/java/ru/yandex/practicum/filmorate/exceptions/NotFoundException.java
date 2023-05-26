d6dd5d23cca9aac2e26cba184028c8a82864ae64
LapshYP <qqqqqqzqqqqqq@yandex.ru>
2023-05-26 11:49:08 +0300

    add-search-2

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/NotFoundException.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.exceptions;
 import org.springframework.http.HttpStatus;
 
 public class NotFoundException extends Throwable {
-    public NotFoundException(HttpStatus badRequest, String wrong_parameters) {
-    }
+    public NotFoundException(HttpStatus badRequest, String msg) {
+
+       }
 }
