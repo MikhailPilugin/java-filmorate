13e4aa06ce99fcc3f93cba16892bea8ae07888d6
LapshYP <qqqqqqzqqqqqq@yandex.ru>
2023-05-26 11:17:33 +0300

    add-director-1

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmServiceImpl.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Service;
